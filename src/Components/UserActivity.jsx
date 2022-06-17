205cbf9fa56dc1fa4bc1bd73a1f7488210f7b9f9
Jay Shah <jayshah06162@gmail.com>
2022-06-17 14:04:24 +0530

    task-2 done

---
+++ src/Components/UserActivity.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../css/UserActivity.css'
+
+function UserActivity(props) {
+    const {item} = props;
+    return (
+        <div className="single-activity">
+            <p className="user-id">{item.userid}</p>
+            <p className="type">{item.status}</p>
+            <p className="date-time">{item.date_time}</p>
+            <p className="ip-add">{item.userIP}</p>
+            <p className="geolocation">{item.geolocation}</p>
+        </div>
+    )
+}
+
+export default UserActivity
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,28 +1,18 @@
 import React,{useEffect,useState} from 'react'
 import '../css/Dashboard.css'
 import axios from 'axios'
+import UserActivity from '../Components/UserActivity';
 
 function Dashboard() {
+    if(localStorage.getItem('button_state')===null){
+        localStorage.setItem('button_state',false);
+    }
     const [user,setUser] = useState(null);
+    const [activityDbData, setActivityDbData] = useState();
+    const [disable, setDisable] = useState(localStorage.getItem('button_state') === 'false' ? false : true);
+    
     useEffect(()=>{
         const getUserDetails = async() => {
-        // fetch('https://localhost:3001/auth/login/success', {
-        //     method: "GET",
-        //     credentials: "include",
-        //     headers: {
-        //         Accept: "application/json",
-        //         "Content-Type": "application/json",
-        //         "Access-Control-Allow-Credentials": true,
-        //     },
-        // }).then((response)=>{
-        //     if(response.status === 200) {
-        //         return response.json();
-        //     }
-        // }).then((resObject)=>{
-        //     setUser(resObject.user);
-        // }).catch((err)=>{
-        //     console.log(err);
-        // });
         try {
             const response = await fetch('https://localhost:3001/auth/login/success', {
                 method: "GET",
@@ -34,7 +24,7 @@ function Dashboard() {
                 }
             });
             const data = await response.json();
-            setUser(data.user);
+            setUser(data.user); 
         }
         catch(err) {
             console.log(err);
@@ -46,15 +36,88 @@ function Dashboard() {
     useEffect(()=>{
         const storeUser = () => {
             axios.post('https://localhost:3001/auth/addUser', {userid: user.id})
-            .then((res)=>console.log('completed'))
+            .then((res)=>console.log(res.data.message))
             .catch((err)=>console.log(err));    
         } 
        user && storeUser();
     });
+    
+    useEffect(()=>{
+        axios.get('https://localhost:3001/auth/getActivityDbData').then((res)=>setActivityDbData(res.data)).catch((err)=>console.log('getting data probll'));
+    },[]);
+    
+    const [activityData, setActivityData] = useState({});
+    const [ipData, setIpData] = useState();
 
-const logoutHandler = () => {
-    window.open('https://localhost:3001/auth/logout', '_self');
-}
+    useEffect(()=>{
+        const getApiData = () => {
+            axios.get('https://ipgeolocation.abstractapi.com/v1/?api_key=53bc354af1a442eb8f7bc09f0ef6030d')
+            .then(response => {
+                setIpData(response.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        }
+        getApiData();
+    },[disable]);
+
+    useEffect(()=>{
+        const getActivityData = () => {
+            setActivityData({
+                userid: user.id,
+                username: user.displayName,
+                status: disable ? 'Checked Out' : 'Checked In',
+                // date_time: ipData.timezone.current_time,
+                date_time: `${new Date().toString().substring(4,15)} | ${new Date().toString().substring(16,21)}`,
+                userIP: ipData.ip_address,
+                geolocation: `${ipData.longitude}/${ipData.latitude}`,
+                
+            })
+        }
+        user && ipData && getActivityData();
+    },[user,ipData,disable]);
+    
+    const checkInHandler = async () => {
+        await setDisable(!disable);
+        localStorage.setItem('button_state',true);
+        axios.post('https://localhost:3001/auth/addUserActivity', activityData).then((res)=>setActivityDbData(res.data)).catch((err)=>console.log(err));
+        postMessage();
+    }
+
+    const checkOutHandler = () => {
+        setDisable(!disable);    
+        localStorage.setItem('button_state',false);    
+        axios.post('https://localhost:3001/auth/addUserActivity', activityData).then((res)=>setActivityDbData(res.data)).catch((err)=>console.log(err));
+        postMessage();
+    }
+    const postMessage = async() => {
+        const webHookURL = 'https://hooks.slack.com/services/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';
+        const data = {
+            "text": `${activityData.username} ${activityData.status} at ${new Date().toString().substring(16,21)}`
+        }
+        const res = await axios.post(webHookURL, JSON.stringify(data), {
+            withCredentials:false,
+            transformRequest: [(data,headers)=>{
+                delete headers.post["Content-Type"];
+                return data;
+            }]
+        });
+        if(res.status === 200){
+            console.log('message posted successfully');
+        }
+        else{
+            console.log('message is not posted');
+        }
+    }
+
+    useEffect(() => {
+        axios.get('https://localhost:3001/auth/getMessages');
+    },[]);
+
+    const logoutHandler = () => {
+        window.open('https://localhost:3001/auth/logout', '_self');
+    }
 
     return (
         <div className="dashboard-container">
@@ -66,9 +129,14 @@ const logoutHandler = () => {
             <div className="btn-cont">
                 {user ? <h1 className="dashboard-text">Welcome {user.displayName}</h1> : <h1 className="dashboard-text">Welcome</h1>}
                 <div className="attendence-btn-container">
-                    <button className="attendence-btn check-in-btn">Check In</button>
-                    <button className="attendence-btn check-out-btn">Check Out</button>
+                    <button className="attendence-btn check-in-btn" disabled={disable} onClick={checkInHandler}>Check In</button>
+                    <button className="attendence-btn check-out-btn" disabled={!disable} onClick={checkOutHandler}>Check Out</button>
                 </div>
+            </div>
+            <div className="activity-container">
+                {activityDbData && activityDbData.map((item,index)=>{
+                        return index<=5 && <UserActivity key={index} item={item}/>
+                })}
             </div>
         </div>
     )
